78e85a6dad9fbdfc5f9ffadb7304fb43c3b3e5c5
super-archive-1419 <sophzsul@gmail.com>
2019-12-28 02:36:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function logWhisper(HELLO){
   console.log('HELLO'.toLowerCase());
 }
 
-function sayHiToGrandma(hello) {
-if ("hello".toLowerCase() === "hello") {
-return “I can’t hear you!”}
-if ("hello".toUpperCase() === "HELLO" ) {
-return “YES INDEED!”}
-if ("I love you, Grandma."=== "I love you, Grandma." ) {
-return “I love you, too.”}
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+}
+
+
 }
